d44aa6ce1a55fa8374ccb4ba6185ed37489ed220
Lakshman.S <haranhari_38@outlook.com>
2021-06-12 00:47:09 +0530

    Added PWA Support for Application.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 import { TranslateService } from '@ngx-translate/core';
@@ -23,13 +24,15 @@ export class AppComponent implements OnInit, OnDestroy {
     private activatedRoute: ActivatedRoute,
     private titleService: Title,
     private translateService: TranslateService,
-    private i18nService: I18nService
+    private i18nService: I18nService,
+    private ngswUpdate: SwUpdate
   ) {}
 
   ngOnInit() {
     // Setup logger
     if (environment.production) {
       Logger.enableProductionMode();
+      this.initializePWA();
     }
 
     log.debug('init');
@@ -64,4 +67,54 @@ export class AppComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     this.i18nService.destroy();
   }
+
+  //#region PWA
+
+  updateNetworkStatusUI() {
+    if (navigator.onLine) {
+      (document.querySelector('body') as any).style = '';
+    } else {
+      (document.querySelector('body') as any).style = 'filter: grayscale(1)';
+    }
+  }
+
+  initializePWA() {
+    // Checking SW Update Status
+    this.ngswUpdate.available.subscribe((update) => {
+      if (update.type === 'UPDATE_AVAILABLE') {
+        if (confirm('New version available. Load New Version?')) {
+          window.location.reload();
+        }
+      }
+    });
+    this.ngswUpdate.checkForUpdate();
+
+    // Checking Network Status
+    this.updateNetworkStatusUI();
+    window.addEventListener('online', this.updateNetworkStatusUI);
+    window.addEventListener('offline', this.updateNetworkStatusUI);
+
+    if ((navigator as any).standalone === undefined) {
+      if (window.matchMedia('(display-mode: browser').matches) {
+        // We are in the browser
+        window.addEventListener('beforeinstallprompt', (event) => {
+          event.preventDefault();
+          if (confirm('Do you want to install this App?')) {
+            (event as any).prompt();
+            (event as any).userChoice.then((result: any) => {
+              if (result.outcome === 'dismissed') {
+                // TODO: Track no installation
+              } else {
+                // TODO: It was installed
+              }
+            });
+          }
+
+          return false;
+        });
+      }
+    }
+  }
+
+  //#endregion
 }
